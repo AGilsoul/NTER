7155cc7ab4e67b4cfa07a18b33ac341f5372b06e
Alex Gilsoul <54417319+AGilsoul@users.noreply.github.com>
2023-07-15 05:00:44 -0600

    started work on grid filling

--- src/GradAscent.py
+++ src/GradAscent.py
@@ -41,14 +41,20 @@ class gradAscent:
 class AMRGrid:
     def __init__(self, data):
         self.amr_data = data
-        self.cell_pos = None
+        self.cell_pos = [self.amr_data["CellCenters:0"],
+                         self.amr_data["CellCenters:1"],
+                         self.amr_data["CellCenters:2"]]
+        self.cell_grads = [self.amr_data["progress_variable_gx"],
+                           self.amr_data["progress_variable_gy"],
+                           self.amr_data["progress_variable_gz"]]
+        self.cell_prog = self.amr_data['progress_variable']
         self.pos_sorted_unique = None
         self.pos_dif = None
         self.pos_lims = None
         self.num_cells = None
         self.amr_shape = None
         self.cell_grads = None
-        self.amr_pos_grid = None
+        self.amr_prog_grid = None
         self.amr_gradient_grid = None
 
     @staticmethod
@@ -60,16 +66,13 @@ class AMRGrid:
         return data[0], data[-1]
 
     def cellPosToGridIndex(self, pos):
-        return [(pos[i] - self.pos_lims[i][0]) / self.pos_dif[i] for i in range(len(pos))]
+        return [round((pos[i] - self.pos_lims[i][0]) / self.pos_dif[i]) for i in range(len(pos))]
 
     def gridIndexToCellPos(self, index):
         return [index[i] * self.pos_dif[i] + self.pos_lims[i][0] for i in range(len(index))]
 
     # gets shape for full AMR grid
     def setFilledAMRShape(self):
-        self.cell_pos = [self.amr_data["CellCenters:0"],
-                         self.amr_data["CellCenters:1"],
-                         self.amr_data["CellCenters:2"]]
         self.pos_sorted_unique = [sorted(x.unique()) for x in self.cell_pos]
         self.pos_dif = [self.getSortedUniqueDataDif(x) for x in self.pos_sorted_unique]
         self.pos_lims = [self.getSortedUniqueDataLims(x) for x in self.pos_sorted_unique]
@@ -77,16 +80,25 @@ class AMRGrid:
         self.amr_shape = self.num_cells[0], self.num_cells[1], self.num_cells[2]
 
     def fillAMRGrid(self):
-        self.cell_grads = [self.amr_data["progress_variable_gx"],
-                           self.amr_data["progress_variable_gy"],
-                           self.amr_data["progress_variable_gz"]]
         self.setFilledAMRShape()
         shape = self.amr_shape
-        print(f'amr shape: {shape}')
-        self.amr_pos_grid = np.zeros(shape=shape)
-        self.amr_gradient_grid = np.zeros(shape=(3,shape[0], shape[1], shape[2]))
-        print(self.amr_pos_grid)
-        print(f'grad shape: {self.amr_gradient_grid.shape}')
+        self.amr_prog_grid = np.zeros(shape=shape)
+        # self.amr_gradient_grid = np.zeros(shape=(3, shape[0], shape[1], shape[2]))
+        print('filling progress grid ...')
+        # MAKE C++ DLL TO DO THIS, TOO SLOW
+        for i in range(len(self.amr_data)):
+            if i % 10000 == 0:
+                print(i)
+            cur_x = self.cell_pos[0][i]
+            cur_y = self.cell_pos[1][i]
+            cur_z = self.cell_pos[2][i]
+            cur_pos = [cur_x, cur_y, cur_z]
+            grid_index = self.cellPosToGridIndex(cur_pos)
+            gi = grid_index[0]
+            gj = grid_index[1]
+            gk = grid_index[2]
+            self.amr_prog_grid[gi][gj][gk] = self.cell_prog[i]
+        print(self.amr_prog_grid)
 
 
 if __name__ == '__main__':
