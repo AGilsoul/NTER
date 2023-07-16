123e98b95ce89508655ff62766b0ddcd9ab260e0
Alex Gilsoul <54417319+AGilsoul@users.noreply.github.com>
2023-07-15 21:31:45 -0600

    working gradient ascent

--- src/GradAscent.py
+++ src/GradAscent.py
@@ -1,9 +1,11 @@
+import math
 from ctypes import cdll, c_int, c_float, POINTER
-import modin.pandas as pd
+import pandas as pd
 import numpy as np
-from multiprocessing import freeze_support
-from modin import config
-from distributed import Client
+from scipy.interpolate import RegularGridInterpolator
+import matplotlib.pyplot as plt
+from scipy.stats import binned_statistic_2d
+
 from csvToPkl import CSVtoPKL
 
 FloatPtr = POINTER(c_float)
@@ -32,32 +34,33 @@ fillGrid.restype = FloatPtr
 
 
 class GradAscent:
-    def __init__(self, pts, gx, gy, gz, num_pts):
-        self.pts = np.array(pts, dtype=np.float32)
-        self.gx = np.array(gx, dtype=np.float32)
-        self.gy = np.array(gy, dtype=np.float32)
-        self.gz = np.array(gz, dtype=np.float32)
-        self.num_pts = num_pts
+    @staticmethod
+    def perform_gA(pts, gx, gy, gz, num_pts):
+        pts = np.array(pts, dtype=np.float32)
+        gx = np.array(gx, dtype=np.float32)
+        gy = np.array(gy, dtype=np.float32)
+        gz = np.array(gz, dtype=np.float32)
+        num_pts = num_pts
 
-    def perform_gA(self):
-        c_pts = self.pts.ctypes.data_as(FloatPtr)
-        c_gx = self.gx.ctypes.data_as(FloatPtr)
-        c_gy = self.gy.ctypes.data_as(FloatPtr)
-        c_gz = self.gz.ctypes.data_as(FloatPtr)
+        c_pts = pts.ctypes.data_as(FloatPtr)
+        c_gx = gx.ctypes.data_as(FloatPtr)
+        c_gy = gy.ctypes.data_as(FloatPtr)
+        c_gz = gz.ctypes.data_as(FloatPtr)
         gradAscent.argtypes = [FloatPtr,
                                FloatPtr,
                                FloatPtr,
                                FloatPtr,
                                c_int]
         gradAscent.restype = FloatPtr
-        res_pts = gradAscent(c_pts, c_gx, c_gy, c_gz, self.num_pts)
-        res = np.ctypeslib.as_array(res_pts, shape=(self.num_pts*3,))
+        res_pts = gradAscent(c_pts, c_gx, c_gy, c_gz, num_pts)
+        res = np.ctypeslib.as_array(res_pts, shape=(num_pts*3,))
         return res
 
 
 class AMRGrid:
     def __init__(self, data):
         self.amr_data = data
+        print(f'num points: {len(data)}')
         self.cell_pos = [self.amr_data['CellCenters:0'],
                          self.amr_data['CellCenters:1'],
                          self.amr_data['CellCenters:2']]
@@ -65,10 +68,13 @@ class AMRGrid:
                            self.amr_data['progress_variable_gy'],
                            self.amr_data['progress_variable_gz']]
         self.cell_prog = self.amr_data['progress_variable']
+        self.cell_curv = self.amr_data['MeanCurvature_progress_variable']
+        self.x_range = None
+        self.y_range = None
+        self.z_range = None
         self.pos_sorted_unique = None
         self.pos_dif = None
         self.pos_lims = None
-        self.num_cells = None
         self.amr_shape = None
         self.c_x = None
         self.c_y = None
@@ -76,62 +82,78 @@ class AMRGrid:
         self.c_xyz_mins = None
         self.c_xyz_difs = None
         self.c_grid_dims = None
-        self.GA = None
         self.setFilledAMRShape()
         self.amr_prog_grid = self.fillAMRGrid(self.amr_prog_grid, self.cell_prog, 'progress_variable')
-        # self.fillAMRGrid()
-        # print('done!')
+        self.amr_curv_grid = self.fillAMRGrid(self.amr_curv_grid, self.cell_curv, 'curvature')
+        self.amr_gx_grid = self.fillAMRGrid(self.amr_gx_grid, self.cell_grads[0], 'grad x')
+        self.amr_gy_grid = self.fillAMRGrid(self.amr_gy_grid, self.cell_grads[1], 'grad y')
+        self.amr_gz_grid = self.fillAMRGrid(self.amr_gz_grid, self.cell_grads[2], 'grad z')
+
+        print('making interpolators')
+        interp_range = (self.x_range, self.y_range, self.z_range)
+        self.prog_interp = RegularGridInterpolator(interp_range, self.amr_prog_grid)
+        self.curv_interp = RegularGridInterpolator(interp_range, self.amr_curv_grid)
+        self.gx_interp = RegularGridInterpolator(interp_range, self.amr_gx_grid)
+        self.gy_interp = RegularGridInterpolator(interp_range, self.amr_gy_grid)
+        self.gz_interp = RegularGridInterpolator(interp_range, self.amr_gz_grid)
+        print('done!')
+
+    def getGACoords(self):
+        x = self.x_range[round(self.amr_shape[0] / 2)]
+        y = self.y_range[round(self.amr_shape[1] / 2)]
+        z = self.z_range[round(self.amr_shape[2] / 10)]
+        return [x, y, z]
 
     @staticmethod
-    def getSortedUniqueDataDif(data):
-        return data[1] - data[0]
-
-    @staticmethod
-    def getSortedUniqueDataLims(data):
-        return data[0], data[-1]
-
-    def cellPosToGridIndex(self, pos):
-        return [round((pos[i] - self.pos_lims[i][0]) / self.pos_dif[i]) for i in range(len(pos))]
-
-    def gridIndexToCellPos(self, index):
-        return [index[i] * self.pos_dif[i] + self.pos_lims[i][0] for i in range(len(index))]
-
-    # gets shape for full AMR grid
-    def setFilledAMRShape(self):
-        self.pos_sorted_unique = [sorted(x.unique()) for x in self.cell_pos]
-        self.pos_dif = np.array([self.getSortedUniqueDataDif(x) for x in self.pos_sorted_unique], dtype=np.float32)
-        self.pos_lims = np.array([self.getSortedUniqueDataLims(x) for x in self.pos_sorted_unique], dtype=np.float32)
-        self.amr_shape = np.array([round((self.pos_lims[i][1] - self.pos_lims[i][0]) / self.pos_dif[i]) for i in range(3)], dtype=np.int32)
-        self.amr_prog_grid = np.zeros(self.amr_shape)
-
-        self.c_x = self.listToFloatPtr(self.cell_pos[0])
-        self.c_y = self.listToFloatPtr(self.cell_pos[1])
-        self.c_z = self.listToFloatPtr(self.cell_pos[2])
-        xyz_mins = np.array([i[0] for i in self.pos_lims], dtype=np.float32)
-        self.c_xyz_mins = self.listToFloatPtr(xyz_mins)
-        self.c_xyz_difs = self.listToFloatPtr(self.pos_dif)
-        self.c_grid_dims = self.listToIntPtr(self.amr_shape)
-
-    @staticmethod
-    def listToFloatPtr(d):
-        return np.array(d, dtype=np.float32).ctypes.data_as(FloatPtr)
-
-    @staticmethod
-    def listToIntPtr(d):
-        return np.array(d, dtype=np.int32).ctypes.data_as(IntPtr)
+    def pointDist(p1, p2):
+        return np.sqrt(sum([(p1[i] - p2[i])**2 for i in range(len(p1))]))
 
     def gradAscentPoint(self):
-        p_i = 1000
-        p_i_xyz = [self.cell_pos[0][p_i], self.cell_pos[1][p_i], self.cell_pos[2][p_i]]
-        print(f'original coords: {p_i_xyz}')
-        gx = [self.cell_grads[0][p_i]]
-        gy = [self.cell_grads[1][p_i]]
-        gz = [self.cell_grads[2][p_i]]
-        print(f'cur gradient: ({gx},{gy},{gz})')
-        num_pts = 1
-        self.GA = GradAscent(p_i_xyz, gx, gy, gz, num_pts)
-        res_pts = self.GA.perform_gA()
-        print(res_pts)
+        amr_coords = self.getGACoords()
+        xyz = self.cellPosToGridIndex(amr_coords)
+
+        cur_prog = self.amr_prog_grid[xyz[0]][xyz[1]][xyz[2]]
+        cur_curv = self.amr_curv_grid[xyz[0]][xyz[1]][xyz[2]]
+        gx = [self.amr_gx_grid[xyz[0]][xyz[1]][xyz[2]]]
+        gy = [self.amr_gy_grid[xyz[0]][xyz[1]][xyz[2]]]
+        gz = [self.amr_gz_grid[xyz[0]][xyz[1]][xyz[2]]]
+
+        r_arr = [0]
+        prog_arr = [cur_prog]
+        curv_arr = [cur_curv]
+
+        last_point = amr_coords
+
+        while cur_prog < 0.98:
+            cur_point = GradAscent.perform_gA(last_point, gx, gy, gz, 1)
+            r_arr.append(self.pointDist(cur_point, last_point) + r_arr[-1])
+            cur_prog = self.prog_interp(cur_point)[0]
+            cur_curv = self.curv_interp(cur_point)[0]
+            # print(f'new point {cur_point} with prog {cur_prog} and curv {cur_curv}')
+            prog_arr.append(cur_prog)
+            curv_arr.append(cur_curv)
+            gx = self.gx_interp(cur_point)
+            gy = self.gy_interp(cur_point)
+            gz = self.gz_interp(cur_point)
+            last_point = cur_point
+
+        print(f'r: {r_arr}')
+        print(f'prog: {prog_arr}')
+        print(f'curv: {curv_arr}')
+        print(len(r_arr))
+        print(len(prog_arr))
+        print(len(curv_arr))
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        H, x_edges, y_edges, bin_num = binned_statistic_2d(r_arr, prog_arr, values=curv_arr, statistic='mean', bins=[75, 75])
+        H = np.ma.masked_invalid(H)
+        XX, YY = np.meshgrid(x_edges, y_edges)
+        p1 = ax.pcolormesh(XX, YY, H.T)
+        cbar = fig.colorbar(p1, ax=ax, label='curvature')
+        ax.set_xlabel('r')
+        ax.set_ylabel('progress variable ')
+        fig.tight_layout()
+        plt.show()
 
     def fillAMRGrid(self, grid_to_fill, val_to_fill, val_name):
         print(f'filling {val_name} grid ...')
@@ -154,15 +176,60 @@ class AMRGrid:
         print('Done!')
         return grid_to_fill
 
+    @staticmethod
+    def getSortedUniqueDataDif(data):
+        return data[1] - data[0]
+
+    @staticmethod
+    def getSortedUniqueDataLims(data):
+        return data[0], data[-1]
+
+    def cellPosToGridIndex(self, pos):
+        return [round((pos[i] - self.pos_lims[i][0]) / self.pos_dif[i]) for i in range(len(pos))]
+
+    def gridIndexToCellPos(self, index):
+        return [index[i] * self.pos_dif[i] + self.pos_lims[i][0] for i in range(len(index))]
+
+    # gets shape for full AMR grid
+    def setFilledAMRShape(self):
+        self.pos_sorted_unique = [sorted(x.unique()) for x in self.cell_pos]
+        self.pos_dif = np.array([self.getSortedUniqueDataDif(x) for x in self.pos_sorted_unique], dtype=np.float32)
+        self.pos_lims = np.array([self.getSortedUniqueDataLims(x) for x in self.pos_sorted_unique], dtype=np.float32)
+        self.amr_shape = np.array([round((self.pos_lims[i][1] - self.pos_lims[i][0]) / self.pos_dif[i]) + 1 for i in range(3)], dtype=np.int32)
+
+        self.amr_prog_grid = np.zeros(self.amr_shape)
+        self.amr_curv_grid = np.zeros(self.amr_shape)
+        self.amr_gx_grid = np.zeros(self.amr_shape)
+        self.amr_gy_grid = np.zeros(self.amr_shape)
+        self.amr_gz_grid = np.zeros(self.amr_shape)
+
+        self.x_range = self.pos_sorted_unique[0]
+        self.y_range = self.pos_sorted_unique[1]
+        self.z_range = self.pos_sorted_unique[2]
+
+        self.c_x = self.listToFloatPtr(self.cell_pos[0])
+        self.c_y = self.listToFloatPtr(self.cell_pos[1])
+        self.c_z = self.listToFloatPtr(self.cell_pos[2])
+        xyz_mins = np.array([i[0] for i in self.pos_lims], dtype=np.float32)
+        self.c_xyz_mins = self.listToFloatPtr(xyz_mins)
+        self.c_xyz_difs = self.listToFloatPtr(self.pos_dif)
+        self.c_grid_dims = self.listToIntPtr(self.amr_shape)
+
+    @staticmethod
+    def listToFloatPtr(d):
+        return np.array(d, dtype=np.float32).ctypes.data_as(FloatPtr)
+
+    @staticmethod
+    def listToIntPtr(d):
+        return np.array(d, dtype=np.int32).ctypes.data_as(IntPtr)
+
 
 if __name__ == '__main__':
     # CSVtoPKL('combGradCurv01929Data')
     file_name = 'res/combGradCurv01929Data.pkl'
-    client = Client()
-    freeze_support()
-    config.MinPartitionSize.put(128)
     print(f'Reading {file_name} ...')
     amr_data = pd.read_pickle(file_name)
+    print(amr_data.columns)
     print(f'Setting up grid ...')
     grid = AMRGrid(amr_data)
-    # grid.gradAscentPoint()
+    grid.gradAscentPoint()
